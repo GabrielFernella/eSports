b2c65b6f489b1589f294dffc3e84b549356f5b54
Gabriel Fernella <gabrielfernella07@hotmail.com>
2022-09-15 03:31:15 -0300

    create simple server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,19 @@ import { response } from 'express';
 
 const app = express();
 
-app.get('ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([{ id: 1, name: 'teste2' }]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.json([{ id: 1, name: 'teste2' }]);
+});
+
+app.get('/game/:id/ads', (request, response) => {
+  return response.json([{ id: 1, name: 'teste2' }]);
+});
+
+app.get('/ads/:id/discord', (request, response) => {
   return response.json([{ id: 1, name: 'teste2' }]);
 });
 
--- server/steps.md
+++ server/steps.md
@@ -1,10 +1,14 @@
 # Package
 
+hoopscotch
+
 1. npm install express
 2. npm i typescript -D
 3. npm i @types/express -D
 4. npm i ts-node-dev -D
+5. npm i prisma -D
 
 # Commands
 
 1. npx tsc --init
+2. npx prisma init --datasource-provider SQLite
